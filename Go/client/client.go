1b46232aab8e4c507e715d09ae6c0534f60e3e2c
Ryan Clements <ryanclementshax@gmail.com>
2019-04-05 15:35:34 -0400

    Add basic go chat server

---
+++ Go/client/client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func listenForMessages(reader *bufio.Reader, chat chan string) {
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		chat <- msg
+	}
+}
+
+func handleInput(scanner *bufio.Scanner, msg chan string) {
+	for scanner.Scan() {
+		m := scanner.Text()
+		msg <- m
+	}
+}
+
+func main() {
+	msg := make(chan string, 1)
+	chat := make(chan string, 1)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	conn, _ := net.Dial("tcp", "localhost:3000")
+	scanner := bufio.NewScanner(os.Stdin)
+	reader := bufio.NewReader(conn)
+
+	go handleInput(scanner, msg)
+	go listenForMessages(reader, chat)
+
+loop:
+	for {
+		select {
+		case <-sigs:
+			fmt.Println("Closing")
+			break loop
+		case s := <-msg:
+			fmt.Fprintf(conn, s+"\n")
+		case c := <-chat:
+			fmt.Print("> " + c)
+		}
+	}
+}
