c24805282487208a820805922043fccdf3cf8921
Derek Wu <46079953+DerekJongWu@users.noreply.github.com>
2019-04-10 14:04:12 -0400

    Update UserThread.java

--- Java/MultiThreaded Chat/UserThread.java
+++ Java/MultiThreaded Chat/UserThread.java
@@ -6,7 +6,6 @@ import java.util.*;
  * This thread handles connection for each connected client, so the server
  * can handle multiple clients at the same time.
  *
- * @author www.codejava.net
  */
 public class UserThread extends Thread {
     private Socket socket;
