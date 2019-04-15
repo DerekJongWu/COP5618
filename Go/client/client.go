0cac58acea08533a99bd09a002583737b8ff0530
Ryan Clements <ryanclementshax@gmail.com>
2019-04-15 08:54:57 -0400

    Add comments

--- Go/client/client.go
+++ Go/client/client.go
@@ -9,24 +9,32 @@ import (
 	"syscall"
 )
 
+// starts listening for messages from the socket
 func listenForMessages(reader *bufio.Reader, chat chan string) {
 	for {
+		// gets message
 		msg, err := reader.ReadString('\n')
+		// handles error
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		// publishes message
 		chat <- msg
 	}
 }
 
+// listens for input from the console
 func handleInput(scanner *bufio.Scanner, msg chan string) {
 	for scanner.Scan() {
+		// gets message
 		m := scanner.Text()
+		// publishes message
 		msg <- m
 	}
 }
 
+// used in the test folder
 func handleDataInput(data []string, msg chan string) {
 	for i := 0; i < len(data); i++ {
 		m := data[i]
@@ -34,28 +42,38 @@ func handleDataInput(data []string, msg chan string) {
 	}
 }
 
+// starts client
 func main() {
+	// makes channel
 	msg := make(chan string, 1)
 	chat := make(chan string, 1)
 	sigs := make(chan os.Signal, 1)
+	// configures to listen for the cancel signals
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
+	// connects to server
 	conn, _ := net.Dial("tcp", "localhost:3000")
+	// makes readers and writers
 	scanner := bufio.NewScanner(os.Stdin)
 	reader := bufio.NewReader(conn)
 
+	// starts goroutines for handling console and server messages
 	go handleInput(scanner, msg)
 	go listenForMessages(reader, chat)
 
+	// switches on the different signals
 loop:
 	for {
 		select {
 		case <-sigs:
+			// we need to cancel
 			fmt.Println("Closing")
 			break loop
 		case s := <-msg:
+			// we got a message from the console so we send to server
 			fmt.Fprintf(conn, s+"\n")
 		case c := <-chat:
+			// we got a message from the server so we send to console
 			fmt.Print("> " + c)
 		}
 	}
--- Go/server/server.go
+++ Go/server/server.go
@@ -9,6 +9,7 @@ import (
 	"syscall"
 )
 
+// to represent each of the clients
 type Client struct {
 	incoming chan string
 	outgoing chan string
@@ -16,33 +17,44 @@ type Client struct {
 	writer   *bufio.Writer
 }
 
+// handles reading the messages from the client socket
 func (client *Client) Read() {
 	for {
+		// reads the string
 		line, err := client.reader.ReadString('\n')
+		// handles error
 		if err != nil {
 			fmt.Println("A connection closed")
 			return
 		}
+		// publishes message on its incoming channel
 		client.incoming <- line
 	}
 }
 
+// handles writing messages to the client socket
 func (client *Client) Write() {
+	// for each message in the outgoing channel
 	for data := range client.outgoing {
+		// write the message to the buffer and flush
 		client.writer.WriteString(data)
 		client.writer.Flush()
 	}
 }
 
+// starts the read and write goroutines
 func (client *Client) Listen() {
 	go client.Read()
 	go client.Write()
 }
 
+// makes a new client object
 func NewClient(connection net.Conn) *Client {
+	// makes new reader and writer objects based on the socket
 	writer := bufio.NewWriter(connection)
 	reader := bufio.NewReader(connection)
 
+	// makes the new client object
 	client := &Client{
 		incoming: make(chan string),
 		outgoing: make(chan string),
@@ -50,26 +62,34 @@ func NewClient(connection net.Conn) *Client {
 		writer:   writer,
 	}
 
+	// starts the listening
 	client.Listen()
 
 	return client
 }
 
+// object to control the chatroom
 type ChatRoom struct {
 	clients  []*Client
 	joins    chan net.Conn
 	incoming chan string
 }
 
+// broadcasts a message by putting it in all of the clients' outgoing channels
 func (chatRoom *ChatRoom) Broadcast(data string) {
 	for _, client := range chatRoom.clients {
 		client.outgoing <- data
 	}
 }
 
+// puts the client in the chatroom
 func (chatRoom *ChatRoom) Join(connection net.Conn) {
+	// makes a new client
 	client := NewClient(connection)
+	// appends it in the list
 	chatRoom.clients = append(chatRoom.clients, client)
+	// starts a goroutine to configure the incoming messages to be put in the chatroom's incoming
+	// messages to broadcast
 	go func() {
 		for {
 			chatRoom.incoming <- <-client.incoming
@@ -77,48 +97,62 @@ func (chatRoom *ChatRoom) Join(connection net.Conn) {
 	}()
 }
 
+// starts the chatroom
 func (chatRoom *ChatRoom) Listen() {
 	go func() {
 		for {
 			select {
 			case data := <-chatRoom.incoming:
+				// broadcasts message
 				chatRoom.Broadcast(data)
 			case conn := <-chatRoom.joins:
+				// adds client to the room
 				chatRoom.Join(conn)
 			}
 		}
 	}()
 }
 
+// makes new chatroom
 func NewChatRoom() *ChatRoom {
+	// new's up object
 	chatRoom := &ChatRoom{
 		clients:  make([]*Client, 0),
 		joins:    make(chan net.Conn),
 		incoming: make(chan string),
 	}
 
+	// starts listening
 	chatRoom.Listen()
 
 	return chatRoom
 }
 
+// accepts client connections
 func Listen(listener net.Listener, connections chan net.Conn) {
 	for {
+		// accepts a connection
 		conn, err := listener.Accept()
+		// handles error
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		// adds new connection into the channel
 		connections <- conn
 	}
 }
 
 func main() {
+	// makes channels
 	connections := make(chan net.Conn, 1)
 	sigs := make(chan os.Signal, 1)
+	// configures sigs channel to publish sigint and sigterm signals
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// makes new chatroom
 	chatRoom := NewChatRoom()
 
+	// starts listening socket
 	listener, err := net.Listen("tcp", ":3000")
 	if err != nil {
 		fmt.Println(err)
@@ -126,8 +160,10 @@ func main() {
 	}
 	defer listener.Close()
 
+	// starts listening for clients
 	go Listen(listener, connections)
 
+	// accepts connections and listens for cancel signals
 	for {
 		select {
 		case <-sigs:
